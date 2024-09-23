6fb7cbfb06809db6150a8dd5f2befc9c125d6eae
Beltran Moreno <beltranmoreno00@gmail.com>
2024-09-23 13:48:49 +0200

    new env variable

--- utils/socket.js
+++ utils/socket.js
@@ -1,11 +1,15 @@
 import { io } from 'socket.io-client';
 
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000"; // Fallback to localhost if env is not set
+
 // const socket = io('http://localhost:4000', {
 //   autoConnect: false, // Prevent automatic connection
 // });
 
-const socket = io('http://142.93.253.67:4000', {
+const socket = io(backendUrl, {
   autoConnect: false, // Prevent automatic connection
+  secure: true,
+  transports: ["websocket", "polling"],
 });
 
 export default socket;
